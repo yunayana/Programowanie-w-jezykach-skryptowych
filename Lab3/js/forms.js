3c949f3e7dae3dd3eda84c13ea67c91d7c220359
Yana Trotsenko <21232@student.ans-elblag.pl>
2025-05-05 00:32:13 +0200

    cwiczenie 9 z podswietlaniem

--- Lab3/js/forms.js
+++ Lab3/js/forms.js
@@ -5,35 +5,46 @@ function checkForm() {
   const email = document.getElementById("contactEmail").value.trim();
   const message = document.getElementById("contactMessage").value.trim();
 
-  // Reset błędów
   document.getElementById("errorName").classList.add("d-none");
   document.getElementById("errorEmail").classList.add("d-none");
   document.getElementById("errorMessage").classList.add("d-none");
 
-  // Imię
+  document.getElementById("contactName").classList.remove("is-invalid", "is-valid");
+  document.getElementById("contactEmail").classList.remove("is-invalid", "is-valid");
+  document.getElementById("contactMessage").classList.remove("is-invalid", "is-valid");
+
   if (name === "") {
     document.getElementById("errorName").classList.remove("d-none");
+    document.getElementById("contactName").classList.add("is-invalid");
     isValid = false;
+  } else {
+    document.getElementById("contactName").classList.add("is-valid");
   }
 
-  // Email
+
   if (email === "") {
     document.getElementById("errorEmail").innerHTML = "Podanie adresu email jest wymagane!";
     document.getElementById("errorEmail").classList.remove("d-none");
+    document.getElementById("contactEmail").classList.add("is-invalid");
     isValid = false;
   } else {
     const regex = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}$/;
     if (!regex.test(email)) {
       document.getElementById("errorEmail").innerHTML = "Podano nieprawidłowy adres email!";
       document.getElementById("errorEmail").classList.remove("d-none");
+      document.getElementById("contactEmail").classList.add("is-invalid");
       isValid = false;
+    } else {
+      document.getElementById("contactEmail").classList.add("is-valid");
     }
   }
 
-  // Wiadomość
   if (message === "" || message.length > 250) {
     document.getElementById("errorMessage").classList.remove("d-none");
+    document.getElementById("contactMessage").classList.add("is-invalid");
     isValid = false;
+  } else {
+    document.getElementById("contactMessage").classList.add("is-valid");
   }
 
   return isValid;
